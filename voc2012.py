fe449248605765f8b194f604456b4eda6a9496b6
KimRass <rmx1000@yonsei.ac.kr>
2023-08-03 18:51:42 +0900

    Minor changes

--- voc2012.py
+++ voc2012.py
@@ -1,6 +1,5 @@
 from pathlib import Path
 from PIL import Image
-from torch.utils.data import Dataset, DataLoader
 import torchvision.transforms as T
 import torchvision.transforms.functional as TF
 import random
@@ -54,13 +53,13 @@ class VOC2012Dataset(Dataset):
         label = TF.resize(label, size=size)
         w, h = label.size
         padding = (max(0, IMG_SIZE - w), max(0, IMG_SIZE - h))
-        label = TF.pad(label, padding=padding, padding_mode="edge")
+        label = TF.pad(label, padding=padding, padding_mode="constant")
         # "We employ crop size to be $513$ during both training and test on PASCAL VOC 2012 dataset."
         t, l, h, w = T.RandomCrop.get_params(img=label, output_size=(IMG_SIZE, IMG_SIZE))
         label = TF.crop(label, top=t, left=l, height=h, width=w)
         
         image = TF.resize(image, size=size)
-        image = TF.pad(image, padding=padding, padding_mode="edge")
+        image = TF.pad(image, padding=padding, padding_mode="constant")
         image = TF.crop(image, top=t, left=l, height=h, width=w)
         return image, label
 
@@ -69,10 +68,8 @@ class VOC2012Dataset(Dataset):
 
     def __getitem__(self, idx):
         label_path = self.labels[idx]
-        label_path = "/Users/jongbeomkim/Documents/datasets/SegmentationClassAug/2007_000733.png"
+        # label_path = "/Users/jongbeomkim/Documents/datasets/SegmentationClassAug/2007_000733.png"
         label = Image.open(label_path)
-        label.show()
-        np.unique(np.array(label))
 
         img_path = f"""{label_path.parent.parent/"JPEGImages"/label_path.stem}.jpg"""
         image = Image.open(img_path).convert("RGB")
@@ -80,6 +77,7 @@ class VOC2012Dataset(Dataset):
         image, label = self._transform(image=image, label=label)
         image = TF.to_tensor(image)
         image = TF.normalize(image, mean=(0.452, 0.431, 0.399), std=(0.277, 0.273, 0.285))
+
         label = TF.pil_to_tensor(label).long()
         return image, label
 
@@ -89,5 +87,4 @@ if __name__ == "__main__":
     idx = 3
     ds = VOC2012Dataset(root_dir=root_dir)
     image, label = ds[3]
-    # blended = Image.blend(image, label.convert("RGB"), alpha=0.5)
-    # blended.show()
+    image.show()
