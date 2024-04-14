e024637ad0acf69e56fd6a14a958399d7ff33137
KimRass <rmx1000@yonsei.ac.kr>
2024-04-14 20:31:29 +0900

    Rebuilt VOC2012Dataset

--- utils.py
+++ utils.py
@@ -17,7 +17,33 @@ import random
 import os
 import numpy as np
 
-import config
+VOC_CLASS_COLOR = {
+    "background": (0, 0, 0),
+    "aeroplane": (128, 0, 0),
+    "bicycle": (0, 128, 0),
+    "bird": (128, 128, 0),
+    "boat": (0, 0, 128),
+    "bottle": (128, 0, 128),
+    "bus": (0, 128, 128),
+    "car": (128, 128, 128),
+    "cat": (64, 0, 0),
+    "chair": (192, 0, 0),
+    "cow": (64, 128, 0),
+    "diningtable": (192, 128, 0),
+    "dog": (64, 0, 128),
+    "horse": (192, 0, 128),
+    "motorbike": (64, 128, 128),
+    "person": (192, 128, 128),
+    "pottedplant": (0, 64, 0),
+    "sheep": (128, 64, 0),
+    "sofa": (0, 192, 0),
+    "train": (128, 192, 0),
+    "tvmonitor": (0, 64, 128),
+    "GRID": (255, 255, 255),
+}
+VOC_CLASSES = list(VOC_CLASS_COLOR.keys())[: -1]
+N_CLASSES = len(VOC_CLASSES)
+VOC_COLORS = list(VOC_CLASS_COLOR.values())
 
 
 def get_device():
@@ -46,60 +72,6 @@ def get_grad_scaler(device):
     return GradScaler() if device.type == "cuda" else None
 
 
-def get_val_filenames(img_dir):
-    val_txt_path = Path(img_dir).parent/"ImageSets/Segmentation/val.txt"
-    with open(val_txt_path, mode="r") as f:
-        filenames = [l.strip() for l in f.readlines()]
-    return filenames
-
-
-def get_voc2012_trainaug_mean_and_std(img_dir, gt_dir):
-    img_dir = Path(img_dir)
-    gt_dir = Path(gt_dir)
-
-    val_filenames = get_val_filenames(img_dir)
-    cnt = 0
-    sum_rgb = 0
-    sum_rgb_square = 0
-    sum_resol = 0
-    for gt_path in tqdm(list(Path(gt_dir).glob("*.png"))):
-        if gt_path.stem in val_filenames:
-            continue
-        cnt += 1
-
-        img_path = (img_dir/gt_path.stem).with_suffix(".jpg")
-        pil_img = Image.open(img_path)
-        tensor = T.ToTensor()(pil_img)
-        
-        sum_rgb += tensor.sum(dim=(1, 2))
-        sum_rgb_square += (tensor ** 2).sum(dim=(1, 2))
-        _, h, w = tensor.shape
-        sum_resol += h * w
-    mean = torch.round(sum_rgb / sum_resol, decimals=3)
-    std = torch.round((sum_rgb_square / sum_resol - mean ** 2) ** 0.5, decimals=3)
-    print(f"""Total {cnt:,} images found.""")
-    return mean, std
-
-
-def get_image_dataset_mean_and_std(data_dir, ext="jpg"):
-    data_dir = Path(data_dir)
-
-    sum_rgb = 0
-    sum_rgb_square = 0
-    sum_resol = 0
-    for img_path in tqdm(list(data_dir.glob(f"""**/*.{ext}"""))):
-        pil_img = Image.open(img_path)
-        tensor = T.ToTensor()(pil_img)
-        
-        sum_rgb += tensor.sum(dim=(1, 2))
-        sum_rgb_square += (tensor ** 2).sum(dim=(1, 2))
-        _, h, w = tensor.shape
-        sum_resol += h * w
-    mean = torch.round(sum_rgb / sum_resol, decimals=3)
-    std = torch.round((sum_rgb_square / sum_resol - mean ** 2) ** 0.5, decimals=3)
-    return mean, std
-
-
 def get_elapsed_time(start_time):
     return timedelta(seconds=round(time() - start_time))
 
@@ -118,14 +90,14 @@ def visualize_batched_gt(gt, n_cols):
     gt[gt == 255] = 0
     grid = make_grid(gt, nrow=n_cols, pad_value=21)
     grid = Image.fromarray(grid[0].numpy().astype("uint8"), mode="P")
-    grid.putpalette(sum(config.VOC_COLORS, ()))
+    grid.putpalette(sum(VOC_COLORS, ()))
     return grid.convert("RGB")
 
 
 def visualize_batched_image_and_gt(image, gt, n_cols, alpha=0.7):
     image = visualize_batched_image(image, n_cols=n_cols)
     gt = visualize_batched_gt(gt, n_cols=n_cols)
-    return Image.blend(image, gt, alpha=alpha)
+    Image.blend(image, gt, alpha=alpha).show()
 
 
 def modify_state_dict(state_dict, pattern=r"^module.|^_orig_mod."):
--- voc2012.py
+++ voc2012.py
@@ -1,46 +1,116 @@
+import torch
 from torch.utils.data import Dataset, DataLoader
 import torchvision.transforms as T
 import torchvision.transforms.functional as TF
+import albumentations as A
+from albumentations.pytorch import ToTensorV2
+import cv2
 from PIL import Image
 from pathlib import Path
 import random
+import numpy as np
+from tqdm import tqdm
 
-import config
-from utils import (
-    get_val_filenames,
-    get_voc2012_trainaug_mean_and_std,
-    visualize_batched_image_and_gt,
-)
+from utils import visualize_batched_image_and_gt
 
 
 class VOC2012Dataset(Dataset):
-    def __init__(self, img_dir, gt_dir, split="train"):
+    # `get_mean_and_std`
+    def __init__(
+        self,
+        img_dir,
+        gt_dir,
+        img_size=513,
+        mean=(0.457, 0.437, 0.404),
+        std=(0.275, 0.271, 0.284),
+        split="train",
+    ):
         super().__init__()
 
         self.img_dir = Path(img_dir)
+        self.gt_dir = Path(gt_dir)
+        self.img_size = img_size
+        val_filenames = self.get_val_filenames()
+        if mean is None and std is None:
+            self.mean, self.std = self.get_mean_and_std(val_filenames)
+        else:
+            self.mean = mean
+            self.std = std
         self.split = split
 
-        self.gts = list(Path(gt_dir).glob("*.png"))
-        filenames = get_val_filenames(self.img_dir)
+        self.gts = list(self.gt_dir.glob("*.png"))
         if split == "train":
-            self.gts = [i for i in self.gts if i.stem not in filenames]
+            self.gts = [i for i in self.gts if i.stem not in val_filenames]
         elif split == "val":
-            self.gts = [i for i in self.gts if i.stem in filenames]
+            self.gts = [i for i in self.gts if i.stem in val_filenames]
+
+        self.val_transform = self.get_val_transform(
+            img_size=img_size, mean=self.mean, std=self.std,
+        )
+
+    def get_val_filenames(self):
+        val_txt_path = self.img_dir.parent/"ImageSets/Segmentation/val.txt"
+        with open(val_txt_path, mode="r") as f:
+            filenames = [l.strip() for l in f.readlines()]
+        return filenames
+
+    def get_mean_and_std(self, val_filenames):
+        cnt = 0
+        sum_rgb = 0
+        sum_rgb_square = 0
+        sum_resol = 0
+        for gt_path in tqdm(list(self.gt_dir.glob("*.png"))):
+            if gt_path.stem in val_filenames:
+                continue
+            cnt += 1
+
+            img_path = (self.img_dir/gt_path.stem).with_suffix(".jpg")
+            pil_img = Image.open(img_path)
+            tensor = T.ToTensor()(pil_img)
+            
+            sum_rgb += tensor.sum(dim=(1, 2))
+            sum_rgb_square += (tensor ** 2).sum(dim=(1, 2))
+            _, h, w = tensor.shape
+            sum_resol += h * w
+        mean = torch.round(sum_rgb / sum_resol, decimals=3)
+        std = torch.round((sum_rgb_square / sum_resol - mean ** 2) ** 0.5, decimals=3)
+        print(f"""Total {cnt:,} images found.""")
+        return mean, std
+
+    @classmethod
+    def get_val_transform(cls, img_size, mean, std):
+        return A.Compose(
+            [
+                A.LongestMaxSize(max_size=img_size, interpolation=cv2.INTER_AREA),
+                A.PadIfNeeded(
+                    min_height=img_size,
+                    min_width=img_size,
+                    border_mode=cv2.BORDER_CONSTANT,
+                    value=tuple([int(i * 255) for i in mean]),
+                ),
+                A.Normalize(mean=mean, std=std),
+                ToTensorV2(),
+            ],
+        )
 
     def _randomly_adjust_b_and_s(self, image):
         image = TF.adjust_brightness(image, random.uniform(0.5, 1.5))
         image = TF.adjust_saturation(image, random.uniform(0.5, 1.5))
         return image
 
-    # "Randomly left-right flipping"
     def _randomly_flip_horizontally(self, image, gt, p=0.5):
+        """
+        "Randomly left-right flipping"
+        """
         if random.random() > 1 - p:
             image = TF.hflip(image)
             gt = TF.hflip(gt)
         return image, gt
 
-    def _randomly_scale(self, image, gt): # Not in the paper
-        # "We apply data augmentation by randomly scaling the input images (from 0.5 to 2.0)."
+    def _randomly_scale(self, image, gt):
+        """
+        "We apply data augmentation by randomly scaling the input images (from 0.5 to 2.0)."
+        """
         w, h = gt.size
         scale = random.uniform(0.5, 2)
         size = (round(scale * h), round(scale * w))
@@ -49,11 +119,14 @@ class VOC2012Dataset(Dataset):
         return image, gt
 
     def _randomly_crop(self, image, gt):
+        """
+        "We employ crop size to be $513$ during both training and test on PASCAL VOC 2012
+            dataset."
+        """
         w, h = gt.size
-        padding = (max(0, config.IMG_SIZE - w), max(0, config.IMG_SIZE - h))
+        padding = (max(0, self.img_size - w), max(0, self.img_size - h))
         gt = TF.pad(gt, padding=padding, padding_mode="constant")
-        # "We employ crop size to be $513$ during both training and test on PASCAL VOC 2012 dataset."
-        t, l, h, w = T.RandomCrop.get_params(img=gt, output_size=(config.IMG_SIZE, config.IMG_SIZE))
+        t, l, h, w = T.RandomCrop.get_params(img=gt, output_size=(self.img_size, self.img_size))
         gt = TF.crop(gt, top=t, left=l, height=h, width=w)
 
         image = TF.pad(image, padding=padding, padding_mode="constant")
@@ -61,16 +134,20 @@ class VOC2012Dataset(Dataset):
         return image, gt
 
     def _transform(self, image, gt):
-        if self.split == "train": # 10,582 images and labels
+        if self.split == "train":
             image = self._randomly_adjust_b_and_s(image)
             image, gt = self._randomly_flip_horizontally(image=image, gt=gt)
             image, gt = self._randomly_scale(image=image, gt=gt)
             image, gt = self._randomly_crop(image=image, gt=gt)
+            image = TF.to_tensor(image)
+            image = TF.normalize(image, mean=self.mean, std=self.std)
+            gt = TF.pil_to_tensor(gt)
 
-        elif self.split == "val": # 1,449 images and labels
-            gt = TF.center_crop(gt, output_size=config.IMG_SIZE)
-            image = TF.center_crop(image, output_size=config.IMG_SIZE)
-        return image, gt
+        elif self.split == "val":
+            transformed = self.val_transform(image=np.array(image), mask=np.array(gt))
+            image = transformed["image"]
+            gt = transformed["mask"][None, ...]
+        return image, gt.long()
 
     def __len__(self):
         return len(self.gts)
@@ -78,17 +155,8 @@ class VOC2012Dataset(Dataset):
     def __getitem__(self, idx):
         gt_path = self.gts[idx]
         gt = Image.open(gt_path)
-
-        img_path = f"""{self.img_dir/gt_path.stem}.jpg"""
-        image = Image.open(img_path).convert("RGB")
-
+        image = Image.open(f"{self.img_dir/gt_path.stem}.jpg").convert("RGB")
         image, gt = self._transform(image=image, gt=gt)
-
-        image = TF.to_tensor(image)
-        # `get_voc2012_trainaug_mean_and_std`
-        image = TF.normalize(image, mean=(0.457, 0.437, 0.404), std=(0.275, 0.271, 0.284))
-
-        gt = TF.pil_to_tensor(gt).long()
         return image, gt
 
 
@@ -97,12 +165,7 @@ if __name__ == "__main__":
     gt_dir = "/Users/jongbeomkim/Documents/datasets/SegmentationClassAug"
     train_ds = VOC2012Dataset(img_dir=img_dir, gt_dir=gt_dir, split="train")
     train_dl = DataLoader(
-        train_ds, batch_size=16, shuffle=True, num_workers=0, pin_memory=True, drop_last=True
+        train_ds, batch_size=4, shuffle=True, num_workers=0, pin_memory=True, drop_last=True
     )
-    cnt = 0
-    for _ in range(10):
-        image, gt = next(iter(train_dl))
-        vis = visualize_batched_image_and_gt(image, gt, n_cols=4, alpha=0.7)
-        vis.show()
-        vis.save(f"""/Users/jongbeomkim/Desktop/workspace/deeplabv3_from_scratch/input_images/{cnt}.jpg""")
-        cnt += 1
+    image, gt = next(iter(train_dl))
+    visualize_batched_image_and_gt(image, gt, n_cols=4, alpha=0.7)
